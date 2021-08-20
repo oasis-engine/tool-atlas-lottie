8871f2dca969dc8f35db698ba9a8999ef43cb0ef
eyworldwide <eyworldwide@foxmail.com>
2021-08-20 20:38:57 +0800

    feat: support url

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const atlasTool = require('@oasis-engine/tool-atlas');
+const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
 
@@ -19,11 +20,22 @@ function createImage(asset, dir) {
   return name;
 }
 
-module.exports = function transform(lottiePath, options = {}) {
-  let rawData = fs.readFileSync(lottiePath);
+module.exports = async function transform(lottiePath, options = {}) {
+  let rawData, data;
+
+  if (lottiePath.startsWith('http://') || lottiePath.startsWith('https://')) {
+    data = await fetch(lottiePath)
+      .then(res => res.json())
+  }
+  else {
+    rawData = fs.readFileSync(lottiePath);
+  }
 
   try {
-    let data = JSON.parse(rawData);
+    if (rawData) {
+      data = JSON.parse(rawData);
+    }
+
     const { nm, assets } = data;
     const { output } = options;
 
@@ -46,17 +58,20 @@ module.exports = function transform(lottiePath, options = {}) {
       }
     }
 
-    return atlasTool.pack(images, { output: `${dir}/${nm}` }).then(() => {
-      console.log('Pack atlas success!')
-      if (fs.existsSync(spritesDir)) {
-        fs.rmdirSync(spritesDir, { recursive: true });
-      }
+    const res = await atlasTool.pack(images, { output: `${dir}/${nm}` });
 
-      // Rewrite lottie json file
-      delete data.assets;
+    console.log('Pack atlas success!')
 
-      fs.writeFileSync(`${dir}/${nm}.json`, JSON.stringify(data));
-    });
+    if (fs.existsSync(spritesDir)) {
+      fs.rmdirSync(spritesDir, { recursive: true });
+    }
+
+    // Rewrite lottie json file
+    delete data.assets;
+
+    fs.writeFileSync(`${dir}/${nm}.json`, JSON.stringify(data));
+
+    return res;
 
   } catch (error) {
     console.log('Parse lottie file error:', error);
